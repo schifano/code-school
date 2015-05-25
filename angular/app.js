a92b1bcf0c9c7b947f4c102e298ebc4323600485
Rachel Schifano <rschifa1@gmail.com>
2015-05-25 01:12:55 -0500

    Initial commit

---
+++ angular/app.js
@@ -0,0 +1,18 @@
+// Controllers - where we define behavior by defining functions and values.
+// Wrapping JavaScript in a closure
+(function() {
+	// Module	Angular JS     App Name Dependencies
+	var app = angular.module('store', []);
+	// Create controller, always name with uppercase
+	// Pass an anonymous function 
+	app.controller('StoreController', function() {
+		// Set property of controller to gem
+		this.product = gem;
+	});
+	// JS object
+	var gem = {
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: '. . .',
+	}
+})();
